5826d363fb0d2110ffbedbacb92555faf30e6f54
Kiran Bisht <kiran.bisht@tothenew.com>
2021-02-03 21:38:35 +0530

    Changed Sweet and Veg Page

--- food/sweet.php
+++ food/sweet.php
@@ -8,7 +8,7 @@ require_once("db.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chinese</title>
+    <title>Chinese Food</title>
 
 
     <link href="foodimg.jpg"rel="icon"type="image/ico">
--- food/veg.php
+++ food/veg.php
@@ -5,7 +5,7 @@ require_once("db.php");
 <!doctype html>
 <html>
 <head>
-<title>veg Page</title>
+<title>VegFood Page</title>
 <link href="foodimg.jpg"rel="icon"type="image/ico">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha256-UzFD2WYH2U1dQpKDjjZK72VtPeWP50NoJjd26rnAdUI=" crossorigin="anonymous" />
